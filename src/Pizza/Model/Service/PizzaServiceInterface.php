ac7e0721e44d488f6358ce883794b3acd0c3ef96
Ralf Eggert <ralf@travello.com>
2015-11-26 17:42:01 +0100

    changed PizzaServiceInterface

--- src/Pizza/Model/Service/PizzaServiceInterface.php
+++ src/Pizza/Model/Service/PizzaServiceInterface.php
@@ -36,11 +36,11 @@ interface PizzaServiceInterface
      * Save vote for a single pizza
      *
      * @param integer $id
-     * @param boolean $vote
+     * @param integer $star
      *
      * @return boolean
      */
-    public function saveVoting($id, $vote);
+    public function saveVoting($id, $star);
 
     /**
      * Save comment for a single pizza
