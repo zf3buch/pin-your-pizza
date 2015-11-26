d35d747d9128ed5ab7aa1c16e1e80e3b1935d350
Ralf Eggert <ralf@travello.com>
2015-11-26 17:40:21 +0100

    Created PizzaTableInterface

---
+++ src/Pizza/Model/Table/PizzaTableInterface.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * ZF3 book Pin Your Pizza Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/pin-your-pizza
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Pizza\Model\Table;
+
+use Zend\Db\TableGateway\TableGatewayInterface;
+
+/**
+ * Interface PizzaTableInterface
+ *
+ * @package Pizza\Model\Table
+ */
+interface PizzaTableInterface extends TableGatewayInterface
+{
+    /**
+     * Fetch all pizzas
+     *
+     * @param integer $count
+     *
+     * @return array
+     */
+    public function fetchAllPizzas($count);
+
+    /**
+     * Fetch pizza by id
+     *
+     * @param integer $id
+     *
+     * @return array
+     */
+    public function fetchPizzaById($id);
+
+    /**
+     * Increase pos column
+     *
+     * @param integer $id
+     * @param integer $star
+     *
+     * @return bool
+     */
+    public function saveVoting($id, $star);
+}
