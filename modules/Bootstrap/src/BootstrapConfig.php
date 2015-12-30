262ad5750e35225282755733169ce964d032294b
Ralf Eggert <ralf@travello.com>
2015-12-30 09:47:09 +0100

    Installed config manager and prepared Bootstrap module

---
+++ modules/Bootstrap/src/BootstrapConfig.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * ZF3 book Pin Your Pizza Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/pin-your-pizza
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Bootstrap;
+
+use Zend\Config\Factory;
+
+/**
+ * Class BootstrapConfig
+ *
+ * @package Bootstrap
+ */
+class BootstrapConfig
+{
+    /**
+     * Define constant
+     */
+    public function __construct()
+    {
+        define('BOOTSTRAP_ROOT', __DIR__ . '/..');
+    }
+
+    /**
+     * Read configuration
+     *
+     * @return array|\Zend\Config\Config
+     */
+    public function __invoke()
+    {
+        return Factory::fromFile(
+            BOOTSTRAP_ROOT . '/config/bootstrap.config.php'
+        );
+    }
+}
