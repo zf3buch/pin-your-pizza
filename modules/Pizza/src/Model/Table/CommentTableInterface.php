5f9750ffe2c0d0c0180248b63a282bcf78f4a8e9
Ralf Eggert <ralf@travello.com>
2016-02-20 13:30:38 +0100

    Table interfaces

---
+++ modules/Pizza/src/Model/Table/CommentTableInterface.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * ZF3 book Pin Your Pizza Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/pin-your-pizza
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Pizza\Model\Table;
+
+/**
+ * Interface CommentTableInterface
+ *
+ * @package Pizza\Model\Table
+ */
+interface CommentTableInterface
+{
+    /**
+     * Fetch comments by pizza id
+     *
+     * @param integer $pizzaId
+     *
+     * @return array
+     */
+    public function fetchCommentsByPizza($pizzaId);
+
+    /**
+     * Save a comment
+     *
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function saveComment(array $data = array());
+
+    /**
+     * Delete a comment
+     *
+     * @param integer $id
+     *
+     * @return mixed
+     */
+    public function deleteComment($id);
+}
---
+++ modules/Pizza/src/Model/Table/PizzaTableInterface.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * ZF3 book Pin Your Pizza Example Application
+ *
+ * @author     Ralf Eggert <ralf@travello.de>
+ * @link       https://github.com/zf3buch/pin-your-pizza
+ * @license    http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
+
+namespace Pizza\Model\Table;
+
+/**
+ * Interface PizzaTableInterface
+ *
+ * @package Pizza\Model\Table
+ */
+interface PizzaTableInterface
+{
+    /**
+     * Fetch all pizzas
+     *
+     * @param integer|null $count
+     *
+     * @return array
+     */
+    public function fetchAllPizzas($count = null);
+
+    /**
+     * Fetch pizza by id
+     *
+     * @param integer $id
+     *
+     * @return array
+     */
+    public function fetchPizzaById($id);
+
+    /**
+     * Increase pos column
+     *
+     * @param integer $id
+     * @param integer $star
+     *
+     * @return bool
+     */
+    public function saveVoting($id, $star);
+}
